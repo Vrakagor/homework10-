aca31130009443ca1975d3f1a70a7cfe68038c27
Olha <1helga.starlight1@gmail.com>
2025-01-20 13:09:42 +0100

    first code commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import keyword
+def is_valid_variable(name):
+    if not name or name in keyword.kwlist or name.count("_") > 1 or name[0].isdigit():
+        return False
+
+    for char in name:
+        if not (char.islower() or char.isdigit() or char == "_"):
+            return False
+    else:
+        return True
+user_input = input("Введіть ім'я змінної: ")
