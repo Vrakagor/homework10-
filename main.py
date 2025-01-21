9c54b7d826e21b1672a12e6e26f0f44111f776e1
Olha <1helga.starlight1@gmail.com>
2025-01-21 15:00:54 +0100

    second code commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import keyword
 def is_valid_variable(name):
-    if not name or name in keyword.kwlist or name.count("_") > 1 or name[0].isdigit():
+    if not name or name in keyword.kwlist or name.count("__") > 1 or name[0].isdigit():
         return False
 
     for char in name:
