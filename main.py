d34b749ca64609bee7db19279b6e18608a0a7cdb
Olha <1helga.starlight1@gmail.com>
2025-01-22 10:17:47 +0100

    fifth code commit

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import keyword
 
+
 def is_valid_variable(name):
-    if not name or name in keyword.kwlist or name[0].isdigit():
+
+    if not name or name in keyword.kwlist:
         return False
 
     if name.strip("_") == "":
         return False
 
-    if "__" in name and not name.lstrip("_").isalnum():
+    if name[0].isdigit():
         return False
 
-    for char in name:
-        if not (char.islower() or char.isdigit() or char == "_"):
-            return False
+    if not all(c.islower() or c.isdigit() or c == "_" for c in name):
+        return False
 
     return True
-
 
 user_input = input("Введіть ім'я змінної: ")
